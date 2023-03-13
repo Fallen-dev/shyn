2382a13019040150f5afbbfc3261f25c3d7024f8
Fallen-dev <rishav.m.19.02@gmail.com>
2023-03-13 15:35:32 +0530

    .gitignore is now working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,2 @@
-<section class="space-y-6">
-	<h1>Welcome to Shyn.</h1>
-
-	<p>New</p>
-	<h2 class="uppercase">Arrivals</h2>
-
-	<div class="card">
-		<h3 class="text-xl font-medium">This is a card</h3>
-		<p>A card title should be 4 to 6 words. And the paragrapg should be 15 to 30 words.</p>
-		<a href="/" role="button" class="btn-outline">Shop now</a>
-	</div>
-
-	<button class="4">Buy now</button>
-
-	<br /><br />
-	<button class="btn-outline">Outline button</button>
-	<button class="">Nromal button</button>
-
-	<br /><br />
-	<button class="btn-outline w-full">Outline button width full</button>
-	<button class="w-full">Nromal button width full</button>
-</section>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
